de0ce3c2c605cbce2f0564e2bef8ad330795961e
lizongti <lzongti@gmail.com>
2022-11-27 00:36:17 +0800

    change warehouse path

--- scripts/dynamic-test.sh
+++ scripts/dynamic-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export dynamic_version="v1.0.2"
+export dynamic_version="v1.0.3"
 docker build -t lambda-dynamic-cli:${dynamic_version} -f ./docker/dynamic-cli.Dockerfile --build-arg VERSION=${dynamic_version} .
-docker run -it --rm -v `pwd`/warehouse:/opt/go-dynamic-warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic1@test
-docker run -it --rm -v `pwd`/warehouse:/opt/go-dynamic-warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic2@test
+docker run -it --rm -v `pwd`/warehouse:/lambda/warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic1@test -w /lambda/warehouse
+docker run -it --rm -v `pwd`/warehouse:/lambda/warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic2@test -w /lambda/warehouse
 
 docker build -t lambda-http-server -f ./docker/http-server.Dockerfile .
 docker stop lambda-http-server || true
--- scripts/lambda-update.sh
+++ scripts/lambda-update.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-docker build -t lambda-http-server -f http.Dockerfile .
-docker run -d --rm --name lambda-http-server -p 8080:8000 -v `pwd`/warehouse:/opt/go-dynamic-warehouse lambda-http-server
-# docker build -t 615170445210.dkr.ecr.us-west-1.amazonaws.com/slots-nano:test .
-# docker push 615170445210.dkr.ecr.us-west-1.amazonaws.com/slots-nano:test
-# sudo aws lambda update-function-code --function-name slots-nano-test --image-uri 615170445210.dkr.ecr.us-west-1.amazonaws.com/slots-nano:test
 
-# sam build
-
-		# go install github.com/aura-studio/dynamic/dynamic-cli@test
-		# dynamic-cli build github.com/aura-studio/testdynamic1@test
-		# dynamic-cli build github.com/aura-studio/testdynamic2@test
-		# go run ./testdynamic@test.go
-
-
+docker build -t 615170445210.dkr.ecr.us-west-1.amazonaws.com/slots-nano:test .
+docker push 615170445210.dkr.ecr.us-west-1.amazonaws.com/slots-nano:test
+sudo aws lambda update-function-code --function-name slots-nano-test --image-uri 615170445210.dkr.ecr.us-west-1.amazonaws.com/slots-nano:test
