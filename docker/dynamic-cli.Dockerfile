e8bd967a75e1f1f29e950102ea7f644c046b5e0e
lizongti <lzongti@gmail.com>
2022-11-27 21:32:30 +0800

    update dynamic-cli

--- docker/dynamic-cli.Dockerfile
+++ docker/dynamic-cli.Dockerfile
@@ -4,7 +4,7 @@ ENV CGO_ENABLED=1
 RUN ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo Asia/Shanghai > /etc/timezone && \
     apt update && apt upgrade -y && apt install -y git gcc g++ ca-certificates golang
 
-ARG VERSION
-RUN	go install github.com/aura-studio/dynamic/dynamic-cli@${VERSION}
+ARG VERSION=v1.0.0
+RUN	go install github.com/aura-studio/dynamic-cli@${VERSION}
 
 ENTRYPOINT ["/root/go/bin/dynamic-cli"]
--- scripts/remote-update.sh
+++ scripts/remote-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export dynamic_version="v1.0.3"
+export dynamic_version="v1.0.0"
 docker build -t lambda-dynamic-cli:${dynamic_version} -f ./docker/dynamic-cli.Dockerfile --build-arg VERSION=${dynamic_version} .
 docker run -it --rm -v `pwd`/warehouse:/lambda/warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic1@test -w /lambda/warehouse
 docker run -it --rm -v `pwd`/warehouse:/lambda/warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic2@test -w /lambda/warehouse
