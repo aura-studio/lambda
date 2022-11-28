b93da125224f459eef76831d61f2fd3483810242
lizongti <lzongti@gmail.com>
2022-11-28 09:48:54 -0800

    fix Dockerfile

--- docker/http-server-image.Dockerfile
+++ docker/http-server-image.Dockerfile
@@ -1,17 +1,9 @@
 FROM ubuntu:22.04
 ENV GOOS=linux
 ENV CGO_ENABLED=1
-ARG S3_REGION
-ARG S3_BUCKET
-ARG S3_ACCESS_KEY
-ARG S3_SECRET_KEY
 WORKDIR /lambda
 RUN ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo Asia/Shanghai > /etc/timezone && \
     apt update && apt upgrade -y && apt install -y git gcc g++ ca-certificates golang
-RUN wget https://github.com/kahing/goofys/releases/latest/download/goofys && \
-    chmod +x goofys && mv goofys /usr/local/bin && \
-    echo "[default]\naws_access_key_id = ${S3_ACCESS_KEY}\naws_secret_access_key = ${S3_SECRET_KEY}\n" > /root/.aws/credentials && \
-    goofys --region ${S3_REGION} ${S3_BUCKET} /lambda/warehouse
 
 COPY . .
 ENV GO_DYNAMIC_WAREHOUSE=/lambda/warehouse
---
+++ scripts/docker-http-server-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export dynamic_version="v1.0.3"
+docker build -t lambda-dynamic-cli:${dynamic_version} -f ./docker/dynamic-cli.Dockerfile --build-arg VERSION=${dynamic_version} .
+docker run -it --rm -v `pwd`/warehouse:/lambda/warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic1@test -w /lambda/warehouse
+docker run -it --rm -v `pwd`/warehouse:/lambda/warehouse lambda-dynamic-cli:${dynamic_version} build github.com/aura-studio/testdynamic2@test -w /lambda/warehouse
+
+docker build -t lambda-http-server:image -f ./docker/http-server-image.Dockerfile .
+docker stop lambda-http-server || true
+docker rm lambda-http-server || true
+docker run -it --rm --name lambda-http-server -p 8080:8000 lambda-http-server:image
