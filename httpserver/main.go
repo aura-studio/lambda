938c83c63ba040a422fbb4364026baf8d0ddb557
lizongti <lzongti@gmail.com>
2022-12-06 22:23:38 +0800

    upgrade dynamic

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,22 +1,8 @@
 package main
 
 import (
-	"os"
-
-	"github.com/aura-studio/dynamic"
 	"github.com/aura-studio/lambda/engine"
 )
-
-func init() {
-	// These environment variables must be set:
-	// S3_REGION/S3_BUCKET/S3_ACCESS_KEY/S3_SECRET_KEY
-	dynamic.WithRemote(dynamic.NewS3Remote(&dynamic.S3Config{
-		Region:    os.Getenv("S3_REGION"),
-		Bucket:    os.Getenv("S3_BUCKET"),
-		AccessKey: os.Getenv("S3_ACCESS_KEY"),
-		SecretKey: os.Getenv("S3_SECRET_KEY"),
-	}))
-}
 
 func main() {
 	engine.ServeHTTP()
