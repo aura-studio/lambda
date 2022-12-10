f1d09c9b903435c4e1510cb17f3c85b8e8c1e4f2
lizongti <lzongti@gmail.com>
2022-12-10 17:39:33 +0800

    update scrips

--- scripts/boostrap-scylladb.sh
+++ scripts/boostrap-scylladb.sh
@@ -1,2 +1,2 @@
 docker volume create scylla-data
-docker run -d --name scylla --hostname scylla --restart=always -p 8001:8001 -v scylla-data:/var/lib/scylla scylladb/scylla --alternator-port 8001
+docker run --name scylla --restart=always -d -p 8000:8000 -v scylla-data:/var/lib/scylla scylladb/scylla-nightly:latest --alternator-port=8000 --alternator-write-isolation=always
