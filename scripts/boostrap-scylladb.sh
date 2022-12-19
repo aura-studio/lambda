a296cddf04641e5f057c5fe0033868cc0bd86232
lizongti <lzongti@gmail.com>
2022-12-19 16:59:51 +0800

    change scylladb port

--- scripts/boostrap-scylladb.sh
+++ scripts/boostrap-scylladb.sh
@@ -1,2 +1,2 @@
 docker volume create scylla-data
-docker run --name scylla --restart=always -d -p 8000:8000 -v scylla-data:/var/lib/scylla scylladb/scylla-nightly:latest --alternator-port=8000 --alternator-write-isolation=always
+docker run --name scylla --restart=always -d -p 18000:18000 -v scylla-data:/var/lib/scylla scylladb/scylla-nightly:latest --alternator-port=18000 --alternator-write-isolation=always
