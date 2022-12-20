2e0ace7d95504ae823d879718ddd6fa3cbcbac9d
lizongti <lzongti@gmail.com>
2022-12-20 14:01:37 +0800

    delete useless

--- scripts/boostrap-scylladb.sh
+++
@@ -1,2 +0,0 @@
-docker volume create scylla-data
-docker run --name scylla --restart=always -d -p 18000:18000 -v scylla-data:/var/lib/scylla scylladb/scylla-nightly:latest --alternator-port=18000 --alternator-write-isolation=always
--- scripts/boostrap-seaweedfs.sh
+++
@@ -1,3 +0,0 @@
-docker volume create seaweedfs-data
-docker volume create seaweedfs-buckets
-docker run -d --name seaweedfs --hostname seaweedfs --restart=always -p 8333:8333 -v seaweedfs-buckets:/buckets -v seawedfs-data:/data chrislusf/seaweedfs server -s3
