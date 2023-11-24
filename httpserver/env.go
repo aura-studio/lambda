80ed50c9b85c6931801bf31a5625940882915da1
lizongti <lzongti@gmail.com>
2023-11-24 17:56:15 +0800

    revert env set

--- httpserver/env.go
+++ httpserver/env.go
@@ -5,9 +5,8 @@ import (
 )
 
 func init() {
-	// os.Setenv("AWS_REGION", os.Getenv("CONFIG_REGION"))
-	// os.Setenv("AWS_ACCESS_KEY_ID", os.Getenv("CONFIG_ACCESS_KEY_ID"))
-	// os.Setenv("AWS_SECRET_ACCESS_KEY", os.Getenv("CONFIG_SECRET_ACCESS_KEY"))
-	//
+	os.Setenv("AWS_REGION", os.Getenv("CONFIG_REGION"))
+	os.Setenv("AWS_ACCESS_KEY_ID", os.Getenv("CONFIG_ACCESS_KEY_ID"))
+	os.Setenv("AWS_SECRET_ACCESS_KEY", os.Getenv("CONFIG_SECRET_ACCESS_KEY"))
 	os.Unsetenv("AWS_SESSION_TOKEN")
 }
