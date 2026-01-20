6512791de97f20063ff6cdd4fc9bde08f0729452
lzongti@gmail.com <lzongti@gmail.com>
2026-01-20 17:26:55 +0800

    feat(sqs): 更新消息协议，调整请求和响应字段名称，修复消息体解码逻辑

---
+++ sqs/sqscli/options.go
@@ -0,0 +1,69 @@
+package sqscli
+
+import (
+	"time"
+
+	"github.com/aura-studio/lambda/sqs"
+	"github.com/mohae/deepcopy"
+)
+
+type Options struct {
+	SQSClient      sqs.SQSClient
+	RequestSqsId   string
+	ResponseSqsId  string
+	DefaultTimeout time.Duration
+	WorkerPoolSize int
+}
+
+type Option interface {
+	Apply(o *Options)
+}
+
+type OptionFunc func(*Options)
+
+func (f OptionFunc) Apply(o *Options) { f(o) }
+
+var defaultOptions = &Options{
+	DefaultTimeout: 30 * time.Second,
+	WorkerPoolSize: 10,
+}
+
+func NewOptions(opts ...Option) *Options {
+	o := deepcopy.Copy(defaultOptions).(*Options)
+	for _, opt := range opts {
+		if opt != nil {
+			opt.Apply(o)
+		}
+	}
+	return o
+}
+
+func WithSQSClient(client sqs.SQSClient) Option {
+	return OptionFunc(func(o *Options) {
+		o.SQSClient = client
+	})
+}
+
+func WithRequestSqsId(id string) Option {
+	return OptionFunc(func(o *Options) {
+		o.RequestSqsId = id
+	})
+}
+
+func WithResponseSqsId(id string) Option {
+	return OptionFunc(func(o *Options) {
+		o.ResponseSqsId = id
+	})
+}
+
+func WithDefaultTimeout(timeout time.Duration) Option {
+	return OptionFunc(func(o *Options) {
+		o.DefaultTimeout = timeout
+	})
+}
+
+func WithWorkerPoolSize(size int) Option {
+	return OptionFunc(func(o *Options) {
+		o.WorkerPoolSize = size
+	})
+}
